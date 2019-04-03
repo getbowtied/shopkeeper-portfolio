c8088c1d8ff967d73c1248722929c3a0afa5bbbf
Vanesa Reinerth <vanesa@getbowtied.com>
2019-04-03 15:37:22 +0300

    ..

--- includes/portfolio/post-type.php
+++ includes/portfolio/post-type.php
@@ -35,7 +35,7 @@ function create_portfolio_item() {
 		'show_in_rest'			=> true,
 		'capability_type' 		=> 'post',
 		'rest_base'				=> 'portfolio-item',
-		'menu_icon'   			=> 'dashicons-portfolio',
+		'menu_icon'   			=> 'data:image/svg+xml;base64,' . base64_encode('<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="black" d="M20 6h-4V4c0-1.11-.89-2-2-2h-4c-1.11 0-2 .89-2 2v2H4c-1.11 0-1.99.89-1.99 2L2 19c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V8c0-1.11-.89-2-2-2zm-6 0h-4V4h4v2z"/></svg>'),
 		'has_archive' 			=> true, 
 		'hierarchical' 			=> true,
 		'menu_position' 		=> 4,
